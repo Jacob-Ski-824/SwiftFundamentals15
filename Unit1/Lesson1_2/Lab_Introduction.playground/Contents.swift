3c1f76d77edbc5c1d52e3a5d6456e64fae0f36f4
Jacob-Ski-824 <Jacob@faski.com>
2025-10-26 15:33:56 -0400

    Update Contents.swift

--- Unit1/Lesson1_2/Lab_Introduction.playground/Contents.swift
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Contents.swift
@@ -12,11 +12,17 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
+print("Song: We Will Rock You by Queen")
 
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
+print("Buddy, you're a boy, make a big noise")
+print("Playing in the street, gonna be a big man someday")
+print("you got mud on your face, you big disgrace")
+print("kickin your can all over the place, singin")
+print("we will, we will ROCK YOU!"
+print("we will, we will ROCK YOU!"
 
 //:page 14 of 16  |  [Next: Exercise: Go! Fight! Win!](@next)

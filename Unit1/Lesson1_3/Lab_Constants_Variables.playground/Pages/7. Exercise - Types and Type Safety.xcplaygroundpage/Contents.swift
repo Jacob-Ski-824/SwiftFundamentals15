238c93d777e6d1f3077d225f5bac711e6f54a8f3
Jacob-Ski-824 <Jacob@faski.com>
2025-10-26 23:17:24 -0400

    Update Contents.swift

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -8,13 +8,16 @@ var secondDecimal = 2.5;
 
 //:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 var trueOrFalse = true;
-print("the two lines of code cannot match up, since one is a numerical value
+print("the two lines of code cannot match up, since one is a numerical value and the other is boolean, there is no type equality and thus the code woudl not compile")
 
 //:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
+var randomString = "Hello";
+(print("same as the previous, but a string, where there is no way to equalize to variables of different types")
+ 
 
 //:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
+var randomNumber = 75
+ print("the code won't compile because the deciaml numbers have the Double type, as opposed to the number type");
 
 /*:
 [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)

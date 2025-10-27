a7ee04cf1e84fbdc6ba112f81f3223e850e58b14
Jacob-Ski-824 <Jacob@faski.com>
2025-10-26 22:31:43 -0400

    Update Contents.swift

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -7,7 +7,8 @@ let friends = 78
 print(friends)
 
 //:  Now assume you go through and remove friends that aren't active on social media. Attempt to update your `friends` constant to a lower number than it currently is. Observe what happens and then move to the next step.
-
+friends = 70
+print(friends)
 
 //:  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friends` constant to a lower number so that the playground will compile properly.
 

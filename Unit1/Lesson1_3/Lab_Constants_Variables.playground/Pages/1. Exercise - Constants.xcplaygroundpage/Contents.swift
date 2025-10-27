0c7ae8b4945fa24e9207faad5a137e1e4b46fdd5
Jacob-Ski-824 <Jacob@faski.com>
2025-10-26 22:41:42 -0400

    Update Contents.swift

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -11,7 +11,7 @@ friends = 70
 print(friends)
 
 //:  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friends` constant to a lower number so that the playground will compile properly.
-
+print("no, by establishing friends as a constant, and having the value of 78, you make it immutable and unable to update, instead, we should use the var keyword to make it able to beunpdaated")
 
 /*:
 page 1 of 10  |  [Next: App Exercise - Step Goal](@next)

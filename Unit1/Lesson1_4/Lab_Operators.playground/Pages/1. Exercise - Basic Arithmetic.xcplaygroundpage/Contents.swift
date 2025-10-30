1b6aecd353437babc2dc6dbb7ce8e8e982bf189f
Jacob-Ski-824 <Jacob@faski.com>
2025-10-29 20:55:17 -0400

    Update Contents.swift

--- Unit1/Lesson1_4/Lab_Operators.playground/Pages/1. Exercise - Basic Arithmetic.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_4/Lab_Operators.playground/Pages/1. Exercise - Basic Arithmetic.xcplaygroundpage/Contents.swift
@@ -3,19 +3,29 @@
  
  You decide to build a shed and want to know beforehand the area of your yard that it will take up. Create two constants, `width` and `height`, with values of 10 and 20, respectively. Create an `area` constant that is the result of multiplying the two previous constants together, and print out the result.
  */
-
+let height = 10
+let width = 20
+let area = height * width
+print(area)
 
 //:  You decide that you'll divide your shed into two rooms. You want to know if dividing it equally will leave enough room for some of your larger storage items. Create a `roomArea` constant that is the result of dividing `area` in half. Print out the result.
-
+let roomArea = area / 2
+print(roomArea)
 
 //:  Create a `perimeter` constant whose value equals `width` plus `width` plus `height` plus `height`, then print out the result.
-
+let perimeter = width +width + height + height
 
 //:  Print what you would expect the result of integer division of 10 divided by 3 to be. Create a constant, `integerDivisionResult` that is the result of 10 divided by 3, and print the value.
-
+print("3.33 repeated");
+let integerDivisionResult = 10 / 3
+print(integerDivisionResult)
 
 //:  Now create two constants, `double10` and `double3`, set to 10 and 3, and declare their types as `Double` values. Declare a final constant `divisionResult` equal to the result of `double10` divided by `double3`. Print the value of `divisionResult`. How does this differ from the value when using integer division?
-
+let double10: Double = 10
+let double3: Double = 3
+let divisionTotal = double10 / double3
+print(divisiontotal);
+print("I think it will still be 3.33 because assigning them doulbe type doesn't double the result")
 
 /*:
  Given the value pi (3.1415927), create a `radius` constant with a value of 5.0, then calculate the diameter and circumference of the circle using the following equations, and print the results:
@@ -25,13 +35,22 @@
  *circumference = 2 * pi * radius.*
  */
 let pi = 3.1415927 
-
+let radius = 5.0
+let circumfrence = (radius * 2) * pi
+let diameter = 2 * radius
+print(circumfrence)
+print(diameter)
 
 //:  Create an integer constant. Using the modulus operator, set its value to the remainder of 12 divided by 5.
-
+let remainder = 12% 5
+print(remainder)
 
 //:  Create two integer constants, `even` and `odd` and set them to any even integer and any odd integer, respectively. For each, print the remainder of dividing the value by 2. Looking at the results, how do you think you could use the remainder operator to determine if an integer is even or odd?
-
+let even = 4
+let odd = 5
+print(even % 2)
+print(odd % 2)
+print("if the answer is 1, that means that the number is odd, because there is a remainder. if it was even the remainder would be 0")
 
 /*:
 page 1 of 8  |  [Next: App Exercise - Fitness Calculations](@next)

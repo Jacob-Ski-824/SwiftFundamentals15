5b101463248893db84ad1f5881f6b6bdf9af3733
Jacob-Ski-824 <Jacob@faski.com>
2025-12-07 23:25:36 -0500

    Add files via upload

---
+++ Unit2/GuidedProject_ApplePie/resources/Apple Pie/Apple Pie Tests/Apple_Pie_3_0Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Pie_3_0Tests.swift
+//  Apple Pie 3.0Tests
+//
+//  Created by Jeffrey Faski on 12/7/25.
+//
+
+import Testing
+@testable import Apple_Pie_3_0
+
+struct Apple_Pie_3_0Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
